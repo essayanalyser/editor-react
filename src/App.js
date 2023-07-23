3133de25c74dd79be8bcf699efd15ccfccd84714
Jayneet123 <68434045+Jayneet123@users.noreply.github.com>
2023-07-23 19:23:05 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import './App.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <h2>Using CKEditor 5 build in React</h2>
+    <CKEditor
+        editor={ ClassicEditor }
+        data="<p>Hello from CKEditor 5!</p>"
+        onReady={ editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log( 'Editor is ready to use!', editor );
+        } }
+        onChange={ ( event, editor ) => {
+            const data = editor.getData();
+            console.log( { event, editor, data } );
+        } }
+        onBlur={ ( event, editor ) => {
+            console.log( 'Blur.', editor );
+        } }
+        onFocus={ ( event, editor ) => {
+            console.log( 'Focus.', editor );
+        } }
+    />
+</div>
   );
 }
 
