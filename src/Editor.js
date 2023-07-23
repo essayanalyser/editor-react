2d5ca57558e4f94da3af51e935e2e4394292d9d5
Jayneet123 <68434045+Jayneet123@users.noreply.github.com>
2023-07-23 19:46:22 +0000

    changes

--- src/Editor.js
+++ src/Editor.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
+import "./App.css"
 
 export default function Editor(){
     return(
-        <div className="App">
-    <h2>Using CKEditor 5 build in React</h2>
+        <div className="Editor">
+    <h2>Welcome to the site !</h2>
     <CKEditor
         editor={ ClassicEditor }
-        data="<p>Hello from CKEditor 5!</p>"
+        data="<p>This is the text editor</p>"
         onReady={ editor => {
             // You can store the "editor" and use when it is needed.
             console.log( 'Editor is ready to use!', editor );
