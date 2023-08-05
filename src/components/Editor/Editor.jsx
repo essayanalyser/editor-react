895438b02a31ebef6e27b5f6daa51dabef74d9fc
Jayneet123 <68434045+Jayneet123@users.noreply.github.com>
2023-08-05 17:44:56 +0530

    ...

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {Button} from 'reactstrap';
+import axios from 'axios';
 
 export default function Editor() {
     const [typedData, setTypedData] = useState('');
-
+    function handleSubmit(e) {
+        e.preventDefault();
+  
+        axios
+            .post("http://localhost:8000/api/users/", {
+                title: 'Its me ',
+                content: this.data,
+            })
+            .then((res) => {
+                this.setState({
+                    title: "",
+                    content: "",
+                });
+            })
+            .catch((err) => {});
+    };
     return (
         <div className="editor">
             <h2>Welcome to the site !</h2>
@@ -23,6 +40,7 @@ export default function Editor() {
 
                 config={{ placeholder: "Start typing..." }}
             />
+            <Button color='info' onClick={handleSubmit}>Submit</Button>
         </div>
     );
 
