fe85132e676f8da3dfae63792880bd46dbaea164
drstone-genius04 <82304884+drstone-genius04@users.noreply.github.com>
2023-08-12 01:47:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Editor from "./components/Editor/Editor"
 import Statistics from "./components/Statistics/Statistics"
-
+import Auth from "./components/Auth/Auth"; // Update the path to auth.js if needed
 import './stylesheets/home.css'
 
 function App() {
