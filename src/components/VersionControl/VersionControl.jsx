4c13cf185d316d70bd4cbc7ef9c8dcd058b7e6be
Aditya <surveaditya521@gmail.com>
2023-10-29 16:43:34 +0530

    new doc button

--- src/components/VersionControl/VersionControl.jsx
+++ src/components/VersionControl/VersionControl.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Logo from "../../assets/logo.svg";
+import NewDocModal from "../modal/NewDocModal";
 
 const VersionControl = ({
   activeVersion,
@@ -19,6 +20,13 @@ const VersionControl = ({
       setData(versions[versions.length - 1].content);
     }
   }, [versions, setActiveVersion, setData]);
+
+  const [showNewDocModal, setShowNewDocModal] = useState(false);
+  const [currentDoc, setCurrentDoc] = useState("");
+
+  const newDocButtonHandler = () => {
+    setShowNewDocModal(true);
+  };
 
   return (
     <div className="h-screen w-1/4 flex flex-col justify-between px-4 py-6">
@@ -46,7 +54,26 @@ const VersionControl = ({
             </div>
           ))}
       </div>
-      <div className="flex items-center justify-between h-12 w-full">
+      <div
+        className="flex items-center justify-center h-16 w-full text-white border-[1px] border-gray-100 border-opacity-20 hover:bg-gray-100 hover:bg-opacity-20 text-xs rounded-md cursor-pointer"
+        onClick={() => {
+          newDocButtonHandler();
+        }}
+      >
+        <div className="w-[20%] h-full flex justify-center items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path d="M19.75 11H12.5V3.75C12.5 3.33579 12.1642 3 11.75 3C11.3358 3 11 3.33579 11 3.75V11H3.75C3.33579 11 3 11.3358 3 11.75C3 12.1642 3.33579 12.5 3.75 12.5H11V19.75C11 20.1642 11.3358 20.5 11.75 20.5C12.1642 20.5 12.5 20.1642 12.5 19.75V12.5H19.75C20.1642 12.5 20.5 12.1642 20.5 11.75C20.5 11.3358 20.1642 11 19.75 11Z" />
+          </svg>
+        </div>
+        <div className="w-[80%] h-full flex items-center">New Doc</div>
+      </div>
+      <div className="flex items-center justify-between mt-5 h-12 w-full">
         <div className="w-32 text-sm truncate text-white">
           {authUser?.email}
         </div>
@@ -65,6 +92,12 @@ const VersionControl = ({
           </>
         )}
       </div>
+      {showNewDocModal && (
+        <NewDocModal
+          setShowNewDocModal={setShowNewDocModal}
+          setCurrentDoc={setCurrentDoc}
+        />
+      )}
     </div>
   );
 };
---
+++ src/components/modal/NewDocModal.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const NewDocModal = ({ setShowNewDocModal, setCurrentDoc }) => {
+  const [value, setValue] = useState("");
+  const changeCurrentDoc = () => {
+    setCurrentDoc(value);
+    setShowNewDocModal(false);
+  };
+  return (
+    <div className="h-screen fixed top-0 left-0 z-50 w-screen bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center">
+      <div className="w-1/3 h-28 bg-white relative rounded-xl flex justify-center items-center p-3">
+        <div className="flex flex-col justify-between h-full px-3 py-1 w-full">
+          <span className="text-[#0084FF] font-bold text-lg">New Document</span>
+          <div
+            onClick={() => {
+              setShowNewDocModal(false);
+            }}
+            className="absolute top-3 right-3 cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M19.7188 18.3906L13.325 12.0004L19.7188 5.65714C20.0392 5.28603 20.0219 4.72911 19.679 4.37894C19.3361 4.02878 18.7832 4.00341 18.4101 4.32073L11.9976 10.6169L5.69734 4.27367C5.33275 3.90878 4.74392 3.90878 4.37933 4.27367C4.20236 4.45039 4.10282 4.69094 4.10282 4.94188C4.10282 5.19282 4.20236 5.43337 4.37933 5.61008L10.6703 11.9439L4.2765 18.2777C4.09954 18.4544 4 18.695 4 18.9459C4 19.1969 4.09954 19.4374 4.2765 19.6141C4.45291 19.7903 4.69172 19.8885 4.94018 19.887C5.18409 19.8885 5.41891 19.794 5.59452 19.6235L11.9976 13.2709L18.4101 19.7271C18.5865 19.9032 18.8253 20.0014 19.0738 20C19.319 19.9989 19.554 19.9009 19.7281 19.7271C19.9039 19.5491 20.0017 19.3078 20 19.0569C19.9982 18.8059 19.897 18.5661 19.7188 18.3906Z" />
+            </svg>
+          </div>
+          <div className="w-full flex gap-4">
+            <input
+              type="text"
+              placeholder="Enter new document name"
+              className="border-[1px] w-[80%] text-gray-700 text-sm border-gray-100 rounded-md px-3 p-2"
+              style={{ boxShadow: "inset 0px 0px 5px 0px rgba(0,0,0,0.25)" }}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <button
+              className="w-[20%] rounded-md bg-[#0084FF] text-white"
+              onClick={() => {
+                changeCurrentDoc();
+              }}
+            >
+              Create
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewDocModal;
