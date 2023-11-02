a78e8a120d06a80d085039262f1bbd37f1fac79b
Aditya <surveaditya521@gmail.com>
2023-11-02 16:45:48 +0530

    editor ui changes

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import "./../../stylesheets/editor.css";
+import { ToolOutlined } from "@ant-design/icons";
 
 export default function Editor({
   handleAnalyseButtonClicked,
@@ -31,7 +32,7 @@ export default function Editor({
 
   return (
     <div className="h-full w-full flex relative justify-center items-center">
-      <div className="h-full w-full flex justify-center items-center">
+      <div className="h-full w-full text-sm flex justify-center items-center">
         <CKEditor
           editor={ClassicEditor}
           data={typedData}
@@ -39,14 +40,17 @@ export default function Editor({
             const data = editor.getData();
             setTypedData(data);
           }}
-          config={{ placeholder: "Start Typing..." }}
+          config={{
+            placeholder: "Start Typing...",
+          }}
         />
       </div>
       <div className="absolute bottom-5 right-7">
         <button
-          className="bg-[#0084FF] hover:scale-105 active:scale-95 text-white px-4 py-2 rounded-md"
+          className="bg-[#0084FF] text-sm gap-2 flex items-center hover:scale-105 active:scale-95 text-white px-4 py-2 rounded-md"
           onClick={() => handleAnalyseButtonClicked(typedData)}
         >
+          <ToolOutlined />
           Analyse
         </button>
       </div>
